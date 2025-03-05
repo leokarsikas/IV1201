b4060e50bab39a65df8fae1aeebe69b6c17c76ef
Olle <151879867+Ollefm@users.noreply.github.com>
2025-03-05 14:33:57 +0100

    application adminView

--- frontend/vite-project/src/pages/LandingPage.tsx
+++ frontend/vite-project/src/pages/LandingPage.tsx
@@ -3,6 +3,7 @@ import Navbar from '../components/navbar';
 import "../styling/Hero.css"
 import heroImage from '../assets/hero-test.png'; // Adjust path to the image
 import Button from '../components/button';
+import { useAuth } from '../hooks/useAuthLogin';
 //import { useAuth } from "../hooks/useAuthLogin";
 
 
@@ -10,23 +11,42 @@ import Button from '../components/button';
 export default  function LandingPage() {
   const navigate = useNavigate();
 
+  const {role, userName} = useAuth();
   function goToApplication(){
     navigate('/application')
   }
 
+  function goToApplicationAsAdmin(){
+    navigate('/recruiter')
+  }
+
   return (
+    <>
+    {role === 1 ? (
       <div className="hero" style={{ backgroundImage: `url(${heroImage})` }}>
+        <Navbar></Navbar>
+       <div className="hero-items">
+        <div className='submit-container'>
+        <h2>Välkommen</h2>
+        <p>Granska och redigera ansökningar genom att trycka på knappen nedan</p>
+        <Button className='submit-container-button' text="Granska ansökningar" onClick={goToApplicationAsAdmin} padding='15px 100px' borderRadius='99px'></Button>
+        </div>
+       </div>
+      </div>)
+      :(<div className="hero" style={{ backgroundImage: `url(${heroImage})` }}>
         <Navbar></Navbar>
        <div className="hero-items">
         <div className='submit-container'>
         <h2>Drömjobbet väntar</h2>
         <p>👉 Sök nu och ta chansen att bli en del av vår framgångssaga!</p>
+       
         <Button className='submit-container-button' text="Ansök nu" onClick={goToApplication} padding='15px 100px' borderRadius='99px'></Button>
         <p style={{color:'white', fontWeight:'500', fontSize:'16px'}}>För att ansöka till våra tjänser måste du vara registrerad</p>
         </div>
        </div>
-      </div>
-    
+      </div>)
+      }
+      </>
   )
 }
 
