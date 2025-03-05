6c5c142da05be2fcd53cf49d77741e3ee83c89f0
Olle <151879867+Ollefm@users.noreply.github.com>
2025-03-05 15:23:06 +0100

    Fixed Token expiration time and ui validation

--- frontend/vite-project/src/pages/LandingPage.tsx
+++ frontend/vite-project/src/pages/LandingPage.tsx
@@ -3,6 +3,7 @@ import Navbar from '../components/navbar';
 import "../styling/Hero.css"
 import heroImage from '../assets/hero-test.png'; // Adjust path to the image
 import Button from '../components/button';
+import { useState } from 'react';
 import { useAuth } from '../hooks/useAuthLogin';
 //import { useAuth } from "../hooks/useAuthLogin";
 
@@ -10,12 +11,15 @@ import { useAuth } from '../hooks/useAuthLogin';
 
 export default  function LandingPage() {
   const navigate = useNavigate();
+  const [showWarning, setShowWarning] = useState(false)
 
   const {role, userName} = useAuth();
   function goToApplication(){
     if(!userName){
-      return alert("LOGGA IN FÖRFAN")
+      setShowWarning(true)
+      return 
     }
+    setShowWarning(false)
     navigate('/application')
   }
 
@@ -41,10 +45,10 @@ export default  function LandingPage() {
        <div className="hero-items">
         <div className='submit-container'>
         <h2>Drömjobbet väntar</h2>
-        <p>👉 Sök nu och ta chansen att bli en del av vår framgångssaga!</p>
+         <p>👉 Sök nu och ta chansen att bli en del av vår framgångssaga!</p>
        
         <Button className='submit-container-button' text="Ansök nu" onClick={goToApplication} padding='15px 100px' borderRadius='99px'></Button>
-        <p style={{color:'white', fontWeight:'500', fontSize:'16px'}}>För att ansöka till våra tjänser måste du vara registrerad</p>
+        {showWarning && <p style={{color:'white', fontWeight:'500', fontSize:'16px'}}>För att ansöka till våra tjänser måste du vara registrerad</p>}
         </div>
        </div>
       </div>)
