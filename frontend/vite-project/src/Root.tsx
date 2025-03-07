e0251bfa30cb90dbe1bbe09a874787faedde0125
Leo Karsikas <leoka@kth.se>
2025-03-07 13:16:40 +0100

    changing root

--- frontend/vite-project/src/Root.tsx
+++ frontend/vite-project/src/Root.tsx
@@ -1,21 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import RegistrationPage from './pages/RegistrationPage.tsx';
 import LoginPage from './pages/LoginPage.tsx';
 import LandingPage from './pages/LandingPage.tsx';
-import RecruiterPage from './pages/RecruiterPage.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-import ApplicationPage from './pages/ApplicationPage.tsx';
 
 function Root() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/application" element={<ApplicationPage />} />
+        <Route path="/application" element={<RegistrationPage />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/recruiter" element={<RecruiterPage />} />
-        <Route path="/error" element={<ErrorPage/>}/>
       </Routes>
     </Router>
   );
