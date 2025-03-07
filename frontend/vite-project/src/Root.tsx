9fece1e05764c84dffa3cb3abc71f20b106057ee
Leo Karsikas <leoka@kth.se>
2025-03-07 13:27:32 +0100

    fix pages

--- frontend/vite-project/src/Root.tsx
+++ frontend/vite-project/src/Root.tsx
@@ -1,6 +1,9 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import RegistrationPage from './pages/RegistrationPage.tsx';
+import ApplicationPage from './pages/ApplicationPage.tsx';
+import RecruiterPage from './pages/RecruiterPage.tsx';
 import LoginPage from './pages/LoginPage.tsx';
+import ErrorPage from './pages/ErrorPage.tsx';
 import LandingPage from './pages/LandingPage.tsx';
 
 function Root() {
@@ -8,9 +11,11 @@ function Root() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/application" element={<RegistrationPage />} />
+        <Route path="/application" element={<ApplicationPage />} />
         <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/recruiter" element={<RecruiterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/error" element={<ErrorPage/>}/>
       </Routes>
     </Router>
   );
